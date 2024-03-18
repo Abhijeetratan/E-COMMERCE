5832ea58697e334f055a8b2d83cd5fbbf6fde5d9
Abhijeetratan <134995703+Abhijeetratan@users.noreply.github.com>
2024-03-18 23:33:37 +0530

    Update productApi.js

--- frontend/src/redux/api/productApi.js
+++ frontend/src/redux/api/productApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const productApi = createApi({
     reducerPath: "productApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }), // Adjust the base URL as needed
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }), 
     keepUnusedDataFor: 30,
     endpoints: (builder) => ({
         getProducts: builder.query({
